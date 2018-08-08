dea55d607ba3fc7c3ca9eee2fb5cbe2f0c907acc
GustavoCig <gustavocignachi@hotmail.com>
2018-08-08 03:16:04 -0300

    Adds the flagging of survivors and refactors code
    Adds methods to update a survivor's number of flags and also update it's is_abducted attribute accordingly.
    Refactors the JSON rendering code, adding a method specific to it.
    
    Commit:Add/Ref
    
    TaskNumber: -

--- app/controllers/survivors_controller.rb
+++ app/controllers/survivors_controller.rb
@@ -26,9 +26,7 @@ class SurvivorsController < ApplicationController
         survivor: @survivor
       }
     else
-      render json: {
-        message: "Error during creation process!"
-      }, status: :bad_request
+      simple_json_message("Error during creation process!", :bad_request)
     end
   end
 
@@ -37,24 +35,46 @@ class SurvivorsController < ApplicationController
 
   def destroy
     if @survivor.destroy
-      render json: {
-        message: "Survivor deleted successfully"
-        }
+      simple_json_message("Survivor deleted successfully")
     else
-      render json: {
-        message: "Error during deletion process!",
-      }, status: :bad_request
+      simple_json_message("Error during deletion process!", :bad_request)
     end
   end
 
   def flag_survivor
+    @survivor = Survivor.find(params[:id])
+    if @survivor.is_abducted
+      simple_json_message("Survivor " + @survivor.name + " already been abducted!")
+    else
+      puts 'TESTE'
+      puts @survivor.number_of_flags
+      increment_survivor_flags(@survivor)
+      puts @survivor.number_of_flags
+      puts update_survivor_status(@survivor)
 
+      message = "Survivor " + @survivor.name
+      message += @survivor.is_abducted ? " has been abducted" : " is still safe..."
+      message += ", flagged " + @survivor.number_of_flags.to_s + " time(s)"
+      puts message
+      
+      if @survivor.update(flagging_params)
+        simple_json_message(message)
+      else
+        simple_json_message("Error during survivor's parameters update!", :bad_request)
+      end
+    end
   end
 
   def show_percentage_abducted
   end
 
   private
+
+  def simple_json_message(message, status=:ok)
+    render json: {
+      message: message
+    }, status: status
+  end
 
   def create_survivor_params
     params.require(:survivor).permit(:name, :age, :gender, :latitude, :longitude)
@@ -65,7 +85,17 @@ class SurvivorsController < ApplicationController
   end
 
   def flagging_params
-    params.require(:survivor).permit(:number_of_flags)
+    params.fetch(:survivor, {}).permit(:number_of_flags, :is_abducted)
+  end
+
+  def increment_survivor_flags(survivor)
+    survivor.number_of_flags += 1
+  end
+
+  def update_survivor_status(survivor)
+    if survivor.number_of_flags >= 3
+      survivor.is_abducted = true
+    end
   end
 
   def find_survivor
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   resources :survivors
+  get '/survivors/:id/flag', to: 'survivors#flag_survivor'
 end
