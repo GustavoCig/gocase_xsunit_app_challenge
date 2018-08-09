bd015f5b4c2e3161cb92975efbe096cc651a743b
GustavoCig <gustavocignachi@hotmail.com>
2018-08-09 11:27:06 -0300

    Adds API versioning
    Commit: Add
    
    TaskNumber: -

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
-  get '/survivors/statistics', to: 'survivors#show_survivors_statistics'
-  resources :survivors
-  get '/survivors/:id/flag', to: 'survivors#flag_survivor'
+  namespace :v1 do
+    root 'welcome#index'
+    get '/survivors/statistics', to: 'survivors#show_survivors_statistics'
+    resources :survivors
+    get '/survivors/:id/flag', to: 'survivors#flag_survivor'
+  end
 end
