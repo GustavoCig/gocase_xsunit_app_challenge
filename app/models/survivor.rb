30d125a5e73ca83d083ce5a129ed0ee6a807e31f
GustavoCig <gustavocignachi@hotmail.com>
2018-08-09 15:27:51 -0300

    Adds uniqueness restraint to Survivor's id in it's model
    Commit: Add
    
    TaskNumber:-

--- app/models/survivor.rb
+++ app/models/survivor.rb
@@ -1,6 +1,6 @@
 class Survivor < ApplicationRecord
     validates :name, :age, :gender, :latitude, :longitude, presence: true
-    validates :name, uniqueness: true
+    validates_uniqueness_of :id, :name
     validates :age, numericality: {only_integer: true}
     validates_inclusion_of :gender, :in => ["male", "female"]
 end
